977e48fbe2421b9109d61c0d23248ee955e6cbee
Sumeja Veseli <94646360+SumejaVeseli@users.noreply.github.com>
2022-05-31 23:45:02 +0200

    Update group.js

--- js/group.js
+++ js/group.js
@@ -1,1 +1,13 @@
 
+console.log('grouping');
+
+function CreateGroupForm() {
+    document.getElementById('join_group_form').style.display = "none";
+    document.getElementById('create_group_form').style.display = "";
+}
+
+function JoinGroupForm() {
+    document.getElementById('create_group_form').style.display = "none";
+    document.getElementById('join_group_form').style.display = "";
+
+}
