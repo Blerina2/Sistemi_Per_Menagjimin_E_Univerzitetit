53e272261e1e8dbca0584c7b19cdc05986bc3e64
Sumeja Veseli <94646360+SumejaVeseli@users.noreply.github.com>
2022-05-31 23:44:17 +0200

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,18 @@
 
+console.log("calender script");
+$('#calendar').calendar({
+    type: "date",
+    initialDate: new Date(),
+    eventDates: [{
+
+            date: new Date(),
+            message: 'Today',
+            class: 'teal'
+        },
+        {
+            date: new Date('2021-11-21'),
+            message: 'School Festival',
+            class: 'green'
+        }
+    ]
+});
