96f5e1aa5e4514ed17ab50d029fa73ab278bc226
Blerina <93825596+Blerina2@users.noreply.github.com>
2022-05-31 22:48:10 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,11 @@
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "e_classrooms";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
