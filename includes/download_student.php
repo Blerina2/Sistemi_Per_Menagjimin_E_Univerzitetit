f85255fdea6754de7cddff27f1e2d65a000f809e
Blerina <93825596+Blerina2@users.noreply.github.com>
2022-05-31 22:48:59 +0200

    Update download_student.php

--- includes/download_student.php
+++ includes/download_student.php
@@ -1,1 +1,35 @@
 
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "e_classrooms";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+include_once 'config.php';
+$download_file_id=$_POST['file_id'];
+$query=mysqli_query($conn, "Select * from student_submission where submission_file_id='$download_file_id'");
+$result = mysqli_fetch_array($query, MYSQLI_ASSOC);
+$filepath = '../../uploads/' . $download_file_id;
+$filepath = $filepath . "." . $result['submission_file_extension'];
+
+$name = $download_file_id . "." . $result['submission_file_extension'];
+
+
+
+
+
+if (file_exists($filepath)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . $result['submission_file_name'] . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize('uploads/' .$name ));
+    readfile('uploads/' . $name);
+}
+?>
