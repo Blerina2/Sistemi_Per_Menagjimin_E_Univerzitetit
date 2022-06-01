91feeff528d77b87edcdbea488101c0d4c248a1c
Arbreshass <93888146+Arbreshass@users.noreply.github.com>
2022-06-01 23:18:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
+<!DOCTYPE html>
 
+<head>
+    <title>Classroom</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" integrity="sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <link rel="stylesheet" href="stylesheets/index.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
+
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <!-- Header -->
+    <div class="ui top fixed menu" style="background-color: lightblue">
+        <div class="item" >
+            <img src="images/panda.png" style="width: 50px">
+        </div>
+        <a class="item" href="index.php">
+            <div class="ui large header">ABS</div>
+        </a>
+        <div class="right menu">
+            <a class="item" href="views/login.php">
+                <div class="ui header">Login</div>
+            </a>
+            <a class="item" href="views/register.php">
+                <div class="ui header">Register</div>
+            </a>
+        </div>
+    </div>
+    <br><br>
+    <div class="ui placeholder teal segment">
+        <div class="heading-container">
+            <div class="ui grid">
+                <div class="nine wide column">
+                    <div class="heading">
+                        <div class="ui teal header">Welcome to ABS!</div>
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="ui small grey header">
+                        Sekreti i suksesit është të bësh gjërat e zakonshme jashtëzakonisht mirë.
+                    </div>
