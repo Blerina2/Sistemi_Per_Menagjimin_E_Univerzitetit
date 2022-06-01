01bc1884df65d73c64937a46fcdf19cc90666c09
Blerina <93825596+Blerina2@users.noreply.github.com>
2022-06-01 23:50:27 +0200

    Create header.php

---
+++ views/partials/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Classroom</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" integrity="sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+</head>
+
+<body>
+
+    <div class="ui top fixed menu" style="background-color: lightblue">
+        <div class="item">
+            <img src="../images/panda.png" style="width: 50px">
+        </div>
+        <a class="item" href="../index.php">
+            <div class="ui large header">ABS </div>
+        </a>
+        <div class="right menu">
+            <a class="item" href="login.php">
+                <div class="ui header">Login</div>
+            </a>
+            <a class="item" href="register.php">
+                <div class="ui header">Register</div>
+            </a>
+        </div>
+    </div>
+
+    <br><br><br><br>
